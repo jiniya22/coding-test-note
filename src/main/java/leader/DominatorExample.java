b6e88e21c295a9eb8576b94c19fca929fe209f36
jini <jini@jiniworld.me>
2023-01-27 12:34:57 +0900

    :sparkles: [codility] leader - dominator, equileader, prime number - Count Factors, Flags, Min Perimeter Rectangle

---
+++ src/main/java/leader/DominatorExample.java
@@ -0,0 +1,36 @@
+package leader;
+
+public class DominatorExample {
+    public static void main(String[] args) {
+        System.out.println(dominator(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+        System.out.println(dominator(new int[]{1, 4, 1, 2, 1}));
+        System.out.println(dominator(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+        System.out.println(dominator(new int[]{0,0,1,1}));
+    }
+
+    static int dominator(int[] A) {
+        if(A == null) return -1;
+        int leader = 0, size = 0;
+        for (int a : A) {
+            if (size == 0) {
+                size++;
+                leader = a;
+            } else {
+                if (leader == a) {
+                    size++;
+                } else {
+                    size--;
+                }
+            }
+        }
+        if(size < 1) return -1;
+        int count = 0, index = -1;
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] == leader) {
+                count++;
+                index = i;
+            }
+        }
+        return count > A.length/2 ? index : -1;
+    }
+}
---
+++ src/main/java/leader/EquiLeaderExample.java
@@ -0,0 +1,46 @@
+package leader;
+
+public class EquiLeaderExample {
+    public static void main(String[] args) {
+//        System.out.println(equiLeader(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+        System.out.println(equiLeader(new int[]{4, 3, 4, 4, 4, 2}));
+        System.out.println(equiLeader(new int[]{4, 4, 2, 5, 3, 4, 4, 4}));
+//        System.out.println(equiLeader(new int[]{0,0,1,1}));
+    }
+
+    static int equiLeader(int[] A) {
+        int leader = 0, size = 0;
+        for (int a : A) {
+            if (size == 0) {
+                size++;
+                leader = a;
+            } else {
+                if (leader == a) {
+                    size++;
+                } else {
+                    size--;
+                }
+            }
+        }
+        if(size < 1) return 0;
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] == leader) {
+                count++;
+            }
+        }
+        if(count <= A.length/2)
+            return 0;
+        int result = 0, temp = 0;
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] == leader) {
+                temp++;
+            }
+            if((i + 1) / 2 < temp && (A.length - (i + 1))/2 < count - temp) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
